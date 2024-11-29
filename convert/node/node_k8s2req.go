01f6d298a4b0ca13e810954c924de3ae53a65393
李永奎 <75-liyongkui@users.noreply.192.168.90.103>
2024-11-29 15:48:56 +0800

    添加备注

--- convert/node/node_k8s2req.go
+++ convert/node/node_k8s2req.go
@@ -27,6 +27,20 @@ func getNodeIp(addresses []corev1.NodeAddress, addressType corev1.NodeAddressTyp
 	}
 	return "<none>"
 }
+
+// GetNodeResItem 方法将 Kubernetes 的 Node 对象转换为自定义的 node_res.Node 类型对象。
+//
+// 参数:
+//
+//	nodeK8s - corev1.Node 类型的参数，表示要转换的 Kubernetes 节点对象。
+//
+// 返回值:
+//
+//	node_res.Node - 转换后的自定义节点对象，包含了节点的详细信息。
+//
+// 说明:
+// 该方法首先从输入的 Kubernetes 节点对象中提取相关信息，然后创建一个自定义的 node_res.Node 类型对象，
+// 并填充该对象的各个字段，最后返回这个自定义对象。
 func (*NodeK8s2Res) GetNodeResItem(nodeK8s corev1.Node) node_res.Node {
 	nodeInfo := nodeK8s.Status.NodeInfo
 	return node_res.Node{
@@ -51,6 +65,23 @@ func mapToList(m map[string]string) []base.ListMapItem {
 	}
 	return res
 }
+
+// GetNodeDetail 方法用于获取 Kubernetes 节点的详细信息，并将其转换为自定义的 node_res.Node 类型对象。
+//
+// 参数:
+//
+//	nodeK8s - corev1.Node 类型的参数，表示要获取详细信息的 Kubernetes 节点对象。
+//
+// 返回值:
+//
+//	node_res.Node - 转换后的自定义节点对象，包含了节点的详细信息。
+//
+// 说明:
+// 该方法首先调用 GetNodeResItem 方法获取节点的基本信息，并填充到 node_res.Node 类型的对象中。
+// 然后，它计算并设置节点的 Labels 和 Taints 信息，最终返回完整的自定义节点对象。
+//
+// - Labels: 将 Kubernetes 节点的 Labels 转换为 map[string]string 类型的列表。
+// - Taints: 直接使用 Kubernetes 节点的 Taints 信息。
 func (this *NodeK8s2Res) GetNodeDetail(nodeK8s corev1.Node) node_res.Node {
 	nodeRes := this.GetNodeResItem(nodeK8s)
 	//计算label和taint
--- utils/listmapitem.go
+++ utils/listmapitem.go
@@ -9,6 +9,21 @@ func ToMap(item []base.ListMapItem) map[string]string {
 	}
 	return dataMap
 }
+
+// ToList 函数将给定的 map[string]string 类型数据转换为 base.ListMapItem 类型的切片。
+//
+// 参数:
+//
+//	data - map[string]string 类型，表示要转换的键值对数据。
+//
+// 返回值:
+//
+//	[]base.ListMapItem - 返回一个包含 base.ListMapItem 类型的切片，每个元素都包含原始 map 中的一个键值对。
+//
+// 说明:
+// 该函数首先创建一个空的 base.ListMapItem 类型切片。然后，它遍历输入的 map 数据，
+// 对于每个键值对，它创建一个新的 base.ListMapItem 对象，并将其添加到切片中。
+// 最后，返回填充好的切片。
 func ToList(data map[string]string) []base.ListMapItem {
 	list := make([]base.ListMapItem, 0)
 	for k, v := range data {
@@ -18,6 +33,22 @@ func ToList(data map[string]string) []base.ListMapItem {
 	}
 	return list
 }
+
+// ToListWithMapByte 函数将给定的 map[string][]byte 类型数据转换为 base.ListMapItem 类型的切片。
+//
+// 参数:
+//
+//	data - map[string][]byte 类型，表示包含字符串键和字节切片值的映射。
+//
+// 返回值:
+//
+//	[]base.ListMapItem - 返回一个包含 base.ListMapItem 类型的切片，其中每个元素都包含原始映射中的一个键值对。
+//	键保持不变，而值则从字节切片转换为字符串。
+//
+// 说明:
+// 该函数首先创建一个空的 base.ListMapItem 类型切片。然后，它遍历输入的 map 数据，
+// 对于每个键值对，它将字节切片值转换为字符串，并创建一个新的 base.ListMapItem 对象，
+// 将其添加到切片中。最后，返回填充好的切片。
 func ToListWithMapByte(data map[string][]byte) []base.ListMapItem {
 	list := make([]base.ListMapItem, 0)
 	for k, v := range data {
